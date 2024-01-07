86fbe93394efe0f205183ecdd49d2f8b85c0c8bd
yahyaouirim <145239153+yahyaouirim@users.noreply.github.com>
2024-01-07 02:52:18 +0100

    first card REACT JSX

---
+++ week3/Day1/Practice/FirstJSX/List_Dojo/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Hello DOJO !!!</h1>
+      <div className="card">
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
