9da0d75f618a9019bb43b7253099f614b2a79baa
yahyaouirim <145239153+yahyaouirim@users.noreply.github.com>
2024-02-14 11:14:43 +0100

    update App.js

--- week5/Day2/core/Product_Manager1/client/src/App.js
+++ week5/Day2/core/Product_Manager1/client/src/App.js
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import Product from './components/Product';
 import Update from './components/Update';
 import DisplayAllProducts from './components/DisplayAllProducts';
 function App() {
+  const [allProducts, setAllProducts] = useState([])
+
   return (
     <div className="App">
       <Routes>
         {/* Main view */}
       <Route path="/" element={<Form />} default />
       {/* Read All */}
-      <Route path="/products" element={<DisplayAllProducts/>}/>
+      <Route path="/products" element={<DisplayAllProducts allProducts={allProducts} setAllProducts={setAllProducts} />}/>
       {/* get one Product */}
       <Route path="/products/:id" element={<Product />} />
 
